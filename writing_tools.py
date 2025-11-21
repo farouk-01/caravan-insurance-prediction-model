03a281c602bfde01bfa2287434d242dbbeae47fe
farouk_01 <farouk_01@outlook.com>
2025-11-20 22:24:02 -0500

    feature creation : interaction terms

--- writing_tools.py
+++ writing_tools.py
@@ -2,36 +2,32 @@ import pandas as pd
 import numpy as np
 import stats_formula
 
-def crossTab_with_caravan_to_markdown(df, var1, isFreq=False):
+def crossTab_with_caravan_to_markdown(df, var1, wFreq=False, wFreqCumul=False, wCond=False, transpose=False):
     ct = pd.crosstab(df[var1], df['CARAVAN'])
-    prob = ct.div(ct.sum(axis=0), axis=1)
+    freq_tbl = ct.copy()
+    freq_tbl.insert(0, var1, freq_tbl.index)
+    freq_tbl.sort_values(by=1, inplace=True, ascending=False)
 
-    if isFreq:
-        freq_tbl = ct.copy()
-        freq_tbl.insert(0, var1, freq_tbl.index)
-        
+    if wFreq:
         row_totals = freq_tbl[[1]].sum(axis=1)
         grand_total = row_totals.sum()
 
         freq_tbl[f"P({var1})"] = row_totals / grand_total
+        if wFreqCumul:
+            freq_tbl[f'cumul P({var1})'] = freq_tbl[f"P({var1})"].cumsum()
+    if wCond:
+         freq_tbl[f'P(CARAVAN = 1 \\| {var1})'] = freq_tbl[1] / (freq_tbl[1] + freq_tbl[0])
 
-        freq_tbl.drop(columns=0, inplace=True)
-        freq_tbl.sort_values(by=1, inplace=True, ascending=False)
-
-        freq_tbl[f'cumul P({var1})'] = freq_tbl[f"P({var1})"].cumsum()
-
-        freq_tbl.rename(columns={1: 'CARAVAN = 1'}, inplace=True)
-        print(freq_tbl.to_markdown(index=False))
+    freq_tbl.drop(columns=0, inplace=True)
+    freq_tbl.rename(columns={1: 'CARAVAN = 1'}, inplace=True)
+    print(f'**Tableau de frequence de {var1} si CARAVAN=1**')
+    if transpose:
+         freq_tbl.set_index(var1, inplace=True)
+         freq_tbl.columns.name = var1
+         print(freq_tbl.T.to_markdown())
     else:
-
-        counts_row = ['CARAVAN = 1'] + ct.loc[1].tolist()
-        prob_row = [f'P(CARAVAN = 1\\|{var1})'] + [f"{v:.4f}" for v in prob.loc[1].tolist()]
-
-        combined = pd.DataFrame(
-            [counts_row, prob_row],
-            columns=[var1] + ct.columns.tolist()
-        )
-        print(combined.to_markdown(index=False))
+         print(freq_tbl.to_markdown(index=False))
+    
 
 def crossTab_with_var2_to_markdown(df, var1, var2, isProb = False):
     ct = pd.crosstab(df[var1], df[var2])
@@ -48,7 +44,7 @@ def crossTab_with_var2_to_markdown(df, var1, var2, isProb = False):
 
     print(formatted.to_markdown())
 
-def crossTab_norm(var1, var2, df, min_count = 5):
+def crossTab_norm(var1, var2, df, min_count=5, hasDict=False, dict1=None, dict2=None):
         col1 = df[var1]
         col2 = df[var2]
         counts1 = pd.crosstab(col1, col2, df['CARAVAN'] == 1, aggfunc='sum')
@@ -56,12 +52,10 @@ def crossTab_norm(var1, var2, df, min_count = 5):
         ct1 = pd.crosstab(col1, col2,  df['CARAVAN'], aggfunc='mean').fillna(0)
         ct2 = pd.crosstab(col2, col1,  df['CARAVAN'], aggfunc='mean').fillna(0)
 
-        ct1 = stats_formula.esperence_filter(counts1, ct1)
-        ct2 = stats_formula.esperence_filter(counts2, ct2)
+        ct1 = stats_formula.esperence_filter(counts1, ct1, min_count=min_count)
+        ct2 = stats_formula.esperence_filter(counts2, ct2, min_count=min_count)
         diff1 = stats_formula.variation_crossTab(ct1, df, var1)
         diff2 = stats_formula.variation_crossTab(ct2, df, var2)
-        # diff1 = ct1
-        # diff2 = ct2
 
         diff2 = diff2.T
 
@@ -83,9 +77,19 @@ def crossTab_norm(var1, var2, df, min_count = 5):
         crossTab_norm = crossTab_norm.rename(columns={var1: f"{var1}/{var2}"})
         return crossTab_norm.round(4)
 
-def crossTab_norm_to_markdown(var1, var2, df):
-    ct = crossTab_norm(var1, var2, df)
+def crossTab_norm_to_markdown(var1, var2, df, min_count=5, hasDict=False, dict1 = None, dict2 = None):
+    ct = crossTab_norm(var1, var2, df, min_count=min_count)
+    interactions = []
+    for i, row in ct.iterrows():
+        for col in ct.columns[1:]:
+            if row[col] > 0:
+                interactions.append(f"{int(row[ct.columns[0]])}x{int(col)}")
+    if hasDict:
+        if dict1 is not None:
+            ct[ct.columns[0]] = ct[ct.columns[0]].map(lambda x: dict1.get(x, x))
     print(ct.to_markdown(index=False))
+    print(f"\n significant interactions: ({var1}x{var2})", ", ".join(interactions))
+    print()
 
 
 
