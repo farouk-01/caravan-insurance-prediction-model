3ebb9976dbcaf3ad09fa39b45775b422fe1a1877
farouk_01 <farouk_01@outlook.com>
2025-12-28 22:42:22 -0500

    rename ModelConfig.py to model_config.py

---
+++ model_config.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass, asdict
+from typing import Optional
+
+@dataclass
+class ModelConfig:
+    threshold:Optional[float] = None
+    class_weight:Optional[float] = None
+    learning_rate:Optional[float] = None
+    l2_reg:bool = False
+    l1_reg:bool = False
+    lambda_const:Optional[float] = None
+
+    def as_dict(self):
+        return asdict(self)
+
+    def copy_with(self, **updates):
+        return ModelConfig(**{**self.as_dict(), **updates})
+
+    def __str__(self):
+        return (
+            f"ModelConfig("
+            f"learning_rate={self.learning_rate:.4f}, "
+            f"class_weight={self.class_weight:.4f}, "
+            f"threshold={self.threshold:.4f}), "
+            f"l2_reg={self.l2_reg})"
+            f"l1_reg={self.l1_reg})"
+            f"lambda_const={self.lambda_const:.4f})"
+        )
